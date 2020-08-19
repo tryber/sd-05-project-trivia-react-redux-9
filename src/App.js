2a880132c7082cb9d00bef7760ce0310de7fc91d
douglaza <douglaza@gmail.com>
2020-08-19 01:10:48 -0300

    esqueleto da pagina inicial - 4th try

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import GamePage from './pages/GamePage';
 import SettingsPage from './pages/SettingsPage';
+import RankingPage from './pages/RankingPage';
 
 // import logo from './trivia.png';
 
@@ -14,6 +15,7 @@ class App extends Component {
           <Route exact path="/" component={HomePage} />
           <Route path="/game" component={GamePage} />
           <Route path="/settings" component={SettingsPage} />
+          <Route path="/ranking" component={RankingPage} />
         </Switch>
       </BrowserRouter>
     );
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -25,32 +25,25 @@ class HomePage extends Component {
     return (
       <div>
         <form>
-          <label htmlFor="email-input">
-            Email:
+          <label htmlFor="email-input"> Email:
             <input
               data-testid="input-gravatar-email"
               value={email}
               placeholder="Digite seu e-mail"
-              onChange={(e) => this.setState({ email: e.target.value })}
-            />
-          </label>
-          <br />
-          <label htmlFor="name-input">
-            Name:
+              onChange={(e) => this.setState({ email: e.target.value })} />
+          </label> <br />
+          <label htmlFor="name-input"> Name:
             <input
               data-testid="input-player-name"
               value={name}
               placeholder="Digite seu nome"
-              onChange={(e) => this.setState({ name: e.target.value })}
-            />
-          </label>
-          <br />
+              onChange={(e) => this.setState({ name: e.target.value })} />
+          </label> <br />
           <Link to="/game">
             <button
               data-testid="btn-play"
               disabled={this.checkLogin()}
-              onClick={() => setLogin(email, name)}
-            >
+              onClick={() => setLogin(email, name)}>
               Jogar
             </button>
           </Link>
@@ -72,10 +65,10 @@ const mapDispatchToProps = (dispatch) => ({
   setLogin: (email, name) => dispatch(setStatus(email, name)),
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(HomePage);
+export default connect(null, mapDispatchToProps)(HomePage);
 
 HomePage.propTypes = {
   setLogin: PropTypes.func.isRequired,
-  getMail: PropTypes.func.isRequired,
-  getName: PropTypes.func.isRequired,
+  // getMail: PropTypes.func.isRequired,
+  // getName: PropTypes.func.isRequired,
 };
