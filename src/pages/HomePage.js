74925364b8bf1d600ec2fb0f6fb65686f90af19e
douglaza <douglaza@gmail.com>
2020-08-19 18:55:58 -0300

    receive changes

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { setStatus, thunkRequest } from '../actions';
+import { setStatus } from '../actions';
 
 class HomePage extends Component {
   constructor(props) {
@@ -46,25 +46,16 @@ class HomePage extends Component {
     );
   }
 
-  // função que verifica se os campos email e nome foram preenchidos
-  // pra então habilitar o botão "jogar"
+  // função que desabilita o botão se não tiver os campos de email & nome preenchidos
   checkLogin() {
     const { email, name } = this.state;
     if (!email || !name) return true;
     return false;
   }
 
-  // função que é chamada ao clicar o botão "jogar"
-  clickPlayButton(email, name) {
-    const { data, setLogin } = this.props;
-    setLogin(email, name);
-    thunkRequest();
-    console.log(data);
-  }
-
   render() {
     const { name, email } = this.state;
-    // const { setLogin } = this.props;
+    const { setLogin } = this.props;
     return (
       <div>
         <form>
@@ -76,14 +67,14 @@ class HomePage extends Component {
             <button
               data-testid="btn-play"
               disabled={this.checkLogin()}
-              onClick={() => this.clickPlayButton(email, name)}
+              onClick={() => setLogin(email, name)}
               type="button"
             >
               Jogar
             </button>
           </Link>
           <Link to="/settings">
-            <button data-testid="btn-settings" type="button"> Configurações </button>
+            <button data-testid="btn-settings"> Configurações </button>
           </Link>
           <Link to="/ranking">
             Ranking
@@ -94,23 +85,17 @@ class HomePage extends Component {
   }
 }
 
-const mapStateToProps = (state) => ({
-  data: state.requestReducer.data,
-  // getMail: state.loginReducer.email,
-  // getName: state.loginReducer.name,
-});
+// const mapStateToProps = (state) => ({
+//   getMail: state.loginReducer.email,
+//   getName: state.loginReducer.name,
+// });
 
 const mapDispatchToProps = (dispatch) => ({
   setLogin: (email, name) => dispatch(setStatus(email, name)),
-  thunkRequest: () => dispatch(thunkRequest()),
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(HomePage);
+export default connect(null, mapDispatchToProps)(HomePage);
 
 HomePage.propTypes = {
   setLogin: PropTypes.func.isRequired,
-  // thunkRequest: PropTypes.func.isRequired,
-  data: PropTypes.string.isRequired,
-  // getMail: PropTypes.func.isRequired,
-  // getName: PropTypes.func.isRequired,
 };
--- src/services/index.js
+++ src/services/index.js
@@ -1,10 +1,11 @@
 const URL = 'https://opentdb.com/api_token.php?command=request';
+
 // retorno da API
-// {"response_code":0,"response_message":"Token Generated Successfully!",
-// "token":"e40bec2c1a71e539d54f75a62581215702ff8e3579b225ef090aa57d3891ac53"}
+// {"response_code":0,"response_message":"Token Generated Successfully!","token":"e40bec2c1a71e539d54f75a62581215702ff8e3579b225ef090aa57d3891ac53"}
 
 const requestAPI = () => (
   fetch(URL).then((resposta) => (
     resposta.json()
       .then((data) => (resposta.ok ? Promise.resolve(data) : Promise.reject(data))))));
+
 export default requestAPI;
