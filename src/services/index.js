3c53b64e3905ac16d4103e6bcff4368cbee46849
marinabrcls <barcelos.marina@gmail.com>
2020-08-19 18:16:54 -0300

    line with over 100 characters

--- src/services/index.js
+++ src/services/index.js
@@ -1,6 +1,7 @@
 const URL = 'https://opentdb.com/api_token.php?command=request';
 // retorno da API
-// {"response_code":0,"response_message":"Token Generated Successfully!","token":"e40bec2c1a71e539d54f75a62581215702ff8e3579b225ef090aa57d3891ac53"}
+// {"response_code":0,"response_message":"Token Generated Successfully!",
+// "token":"e40bec2c1a71e539d54f75a62581215702ff8e3579b225ef090aa57d3891ac53"}
 
 const requestAPI = () => (
   fetch(URL).then((resposta) => (
