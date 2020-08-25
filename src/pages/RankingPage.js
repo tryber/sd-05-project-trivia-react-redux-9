82a2ec4ce4eaf08b50c884490e2335ac6b1a1e9c
marinabrcls <barcelos.marina@gmail.com>
2020-08-25 18:02:41 -0300

    eslint corrections

--- src/pages/RankingPage.js
+++ src/pages/RankingPage.js
@@ -15,21 +15,19 @@ class RankingPage extends Component {
       <div>
         <Reset />
         <h1 data-testid="ranking-title">Ranking</h1>
-        {ranking.map((user, index) => {
-          return (
-            <div>
-              <img src={`https://www.gravatar.com/avatar/${hash}`} alt={user.name} />
-              <p data-testid={`player-name-${index}`}>
-                Player:
-                {user.name}
-              </p>
-              <p data-testid={`player-score-${index}`}>
-                Score:
-                {user.score}
-              </p>
-            </div>
-          );
-        })}
+        {ranking.map((user, index) => (
+          <div>
+            <img src={`https://www.gravatar.com/avatar/${hash}`} alt={user.name} />
+            <p data-testid={`player-name-${index}`}>
+              Player:
+              {user.name}
+            </p>
+            <p data-testid={`player-score-${index}`}>
+              Score:
+              {user.score}
+            </p>
+          </div>
+        ))}
       </div>
     );
   }
