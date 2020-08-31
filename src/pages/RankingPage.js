ee53ff9e19caed25cff09be72d9daa2c43fac5c9
marinabrcls <barcelos.marina@gmail.com>
2020-08-31 15:46:10 -0300

    ranking page

--- src/pages/RankingPage.js
+++ src/pages/RankingPage.js
@@ -14,36 +14,23 @@ class RankingPage extends Component {
     return (
       <div>
         <Card>
-          <h1 data-testid="ranking-title">Ranking</h1>
-          <div className="wrapper">
-            <table>
-              <thead>
-                <tr>
-                  <th>Jogador</th>
-                  <th>Pontuação</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td data-testid="player-name-0">Eduardo</td>
-                  <td data-testid="player-score-0">50 pontos</td>
-                </tr>
-                <tr>
-                  <td data-testid="player-name-1">Camila</td>
-                  <td data-testid="player-score-1">45 pontos</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
           <Reset />
           <h1 data-testid="ranking-title">Ranking</h1>
-          {rankingSorted.map((user, index) => (
-            <div>
-              <img src={`https://www.gravatar.com/avatar/${hash}`} alt={user.name} />
-              <p>Player:<span data-testid={`player-name-${index}`}>{user.name}</span></p>
-              <p>Score:<span data-testid={`player-score-${index}`}>{user.score}</span></p>
-            </div>
-          ))}
+          <div className="wrapper">
+            {rankingSorted.map((user, index) => (
+              <div>
+                <img src={`https://www.gravatar.com/avatar/${hash}`} alt={user.name} />
+                <p>
+                  Player:
+                  <span data-testid={`player-name-${index}`}>{user.name}</span>
+                </p>
+                <p>
+                  Score:
+                  <span data-testid={`player-score-${index}`}>{user.score}</span>
+                </p>
+              </div>
+            ))}
+          </div>
         </Card>
       </div>
     );
